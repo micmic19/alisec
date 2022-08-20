68d9b6ccdbdbe138adf8f9572f09cbc6978fe4cd
Rudolf Fedorov <52194526+rurraka@users.noreply.github.com>
2022-08-20 16:59:37 +0300

    Уменьшил иконки в мобильной версии

--- include/company_logo.php
+++ include/company_logo.php
@@ -2,8 +2,8 @@
 	<img src="/include/logo3.svg?ver='1.4'" srcset="/include/logo3.svg?ver='1.4'">
 </a>
 <a class="hidden-sm hidden-lg hidden-md" style="padding-left: 42%;" href="https://vk.com/alicenapol">
-	<img style="max-width: 18%;padding: 7px;" src="/images/vk-com-logo-circle.svg" srcset="/images/vk-com-logo-circle.svg">
+	<img style="max-width: 14%;padding: 7px;" src="/images/vk-com-logo-circle.svg" srcset="/images/vk-com-logo-circle.svg">
 </a>
 <a class="hidden-sm hidden-lg hidden-md" href="tel:+78123311461">
-	<img style="max-width: 12%;" src="/images/phone_icon.svg?ver='1.4'" srcset="/images/phone_icon.svg?ver='1.4'">
+	<img style="max-width: 10%;" src="/images/phone_icon.svg?ver='1.4'" srcset="/images/phone_icon.svg?ver='1.4'">
 </a>
