29db94b3697b5263897296dd5b09fab207c9279b
micmic19 <45197167+micmic19@users.noreply.github.com>
2022-08-21 11:08:31 +0300

    add target _blank on vk link

--- include/vk_header.php
+++ include/vk_header.php
@@ -1,3 +1,3 @@
-<a href="https://vk.com/alicenapol">
+<a href="https://vk.com/alicenapol" target="_blank">
 	<img style="max-width: 27px;" src="/images/vk-com-logo-circle.svg" srcset="/images/vk-com-logo-circle.svg">
 </a>
