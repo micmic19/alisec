4682381430ffab076f1c757a0a6f9487f1d30881
micmic19 <45197167+micmic19@users.noreply.github.com>
2022-08-24 12:08:49 +0300

    Some cosmetic changes

--- include/company_logo.php
+++ include/company_logo.php
@@ -1,7 +1,7 @@
 <a class="bx-logo-block" href="<?=SITE_DIR?>">
 	<img src="/include/logo3.svg?ver='1.4'" srcset="/include/logo3.svg?ver='1.4'">
 </a>
-<a class="hidden-sm hidden-lg hidden-md" style="padding-left: 42%;" href="https://vk.com/alicenapol">
+<a class="hidden-sm hidden-lg hidden-md" style="padding-left: 42%;" href="https://vk.com/alicenapol" target="_blank">
 	<img style="max-width: 14%;padding: 7px;" src="/images/vk-com-logo-circle.svg" srcset="/images/vk-com-logo-circle.svg">
 </a>
 <a class="hidden-sm hidden-lg hidden-md" href="tel:+78123311461">
--- index.php
+++ index.php
@@ -78,7 +78,7 @@ $arrFilter = array(array(
 	),
 	false,
 	array(
-	"ACTIVE_COMPONENT" => "N"
+	"ACTIVE_COMPONENT" => "Y"
 	)
 );?>
 
